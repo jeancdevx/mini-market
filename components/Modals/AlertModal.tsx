849abbeef1c4120a131639275e0b86e8dad3a57e
jcodev2 <jcode2006@gmail.com>
2024-05-02 09:34:57 -0500

    feat: :sparkles: add alert modal component

---
+++ components/Modals/AlertModal.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import { Button } from '@/components/ui/Button'
+import Modal from '@/components/ui/Modal'
+
+interface AlertModalProps {
+  isOpen: boolean
+  onClose: () => void
+  onConfirm: () => void
+  loading: boolean
+}
+
+const AlertModal = ({
+  isOpen,
+  onClose,
+  onConfirm,
+  loading
+}: AlertModalProps) => {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+    return () => setIsMounted(false)
+  }, [])
+
+  if (!isMounted) return null
+
+  return (
+    <Modal
+      title='Are you sure?'
+      description='This action cannot be undone.'
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className='flex w-full items-center justify-end space-x-2 pt-6'>
+        <Button
+          disabled={loading}
+          variant='outline'
+          onClick={onClose}
+        >
+          Cancel
+        </Button>
+        <Button
+          onClick={onConfirm}
+          disabled={loading}
+          variant='destructive'
+        >
+          Confirm
+        </Button>
+      </div>
+    </Modal>
+  )
+}
+
+export default AlertModal
