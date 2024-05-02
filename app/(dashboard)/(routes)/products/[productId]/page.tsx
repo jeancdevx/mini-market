4becc032c7ce27fedda7ac478e983c2ea75ff4a2
jcodev2 <jcode2006@gmail.com>
2024-05-02 09:21:43 -0500

    feat: :sparkles: add product id page

---
+++ app/(dashboard)/(routes)/products/[productId]/page.tsx
@@ -0,0 +1,32 @@
+import FormProduct from '@/components/Forms/FormProduct'
+
+import { db } from '@/lib/db'
+
+interface ProductIdPageProps {
+  params: {
+    productId: string
+  }
+}
+
+const ProductIdPage = async ({ params: { productId } }: ProductIdPageProps) => {
+  const product = await db.product.findUnique({
+    where: {
+      id: productId
+    }
+  })
+
+  const categories = await db.category.findMany()
+
+  return (
+    <div className='flex flex-col'>
+      <div className='flex-1 space-y-4 p-8 pt-6'>
+        <FormProduct
+          initialData={product}
+          categories={categories}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ProductIdPage
