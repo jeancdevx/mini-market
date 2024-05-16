3d0fb9cde5e3bdc2fe3a9b299cab704cdd2e40ab
jcodev2 <jcode2006@gmail.com>
2024-05-16 18:28:45 -0500

    refactor: update CategoriesPage component
    Refactor to use the getCategories for fetching categories and remove unused code.

--- app/(dashboard)/(routes)/categories/page.tsx
+++ app/(dashboard)/(routes)/categories/page.tsx
@@ -1,26 +1,14 @@
-import { format } from 'date-fns'
-
-import { db } from '@/lib/db'
+import { getCategories } from '@/server/categories'
 
 import CategoryClient from '@/components/Categories/CategoryClient'
 
 export default async function CategoriesPage() {
-  const categories = await db.category.findMany({
-    orderBy: {
-      createdAt: 'desc'
-    }
-  })
-
-  const formattedCategories = categories.map(({ id, name, createdAt }) => ({
-    id,
-    name,
-    createdAt: format(createdAt, 'MMMM do, yyyy')
-  }))
+  const categories = await getCategories()
 
   return (
     <div className='flex flex-col'>
       <div className='flex-1 p-8 pt-6'>
-        <CategoryClient categories={formattedCategories} />
+        <CategoryClient categories={categories} />
       </div>
     </div>
   )
