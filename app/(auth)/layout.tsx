22ecf1f63b75743d2e4fd998fcb402a1896f14e8
jcodev2 <jcode2006@gmail.com>
2024-05-16 18:29:18 -0500

    refactor: update AuthLayout component
    Update AuthLayout component to use 'h-screen' instead of 'h-full' for better responsiveness.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,6 +4,6 @@ export default function AuthLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className='flex h-full items-center justify-center'>{children}</div>
+    <div className='flex h-screen items-center justify-center'>{children}</div>
   )
 }
