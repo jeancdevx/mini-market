9b6c7cb1c344915f141b8ae697454704c5f0a667
jcodev2 <jcode2006@gmail.com>
2024-05-16 18:27:50 -0500

    refactor: update ProductsPage component
    Refactor to use the getProducts for fetching products and remove unused code.

--- app/(dashboard)/(routes)/products/page.tsx
+++ app/(dashboard)/(routes)/products/page.tsx
@@ -1,35 +1,14 @@
-import { format } from 'date-fns'
-
-import { db } from '@/lib/db'
+import { getProducts } from '@/server/products'
 
 import ProductClient from '@/components/Products/ProductClient'
 
 export default async function ProductsPage() {
-  const products = await db.product.findMany({
-    orderBy: {
-      createdAt: 'desc'
-    },
-    include: {
-      category: true
-    }
-  })
-
-  const formattedProducts = products.map(
-    ({ id, name, image, stock, price, category, createdAt }) => ({
-      id,
-      name,
-      image,
-      stock,
-      price,
-      category: category.name,
-      createdAt: format(createdAt, 'MMMM do, yyyy')
-    })
-  )
+  const products = await getProducts()
 
   return (
     <div className='flex flex-col'>
       <div className='flex-1 p-8 pt-6'>
-        <ProductClient products={formattedProducts} />
+        <ProductClient products={products} />
       </div>
     </div>
   )
