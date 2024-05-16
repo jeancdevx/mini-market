32a75538255c8cb2ee372bc1f0aa556c1102bf9c
jcodev2 <jcode2006@gmail.com>
2024-05-16 18:25:54 -0500

    refactor: update DashboardPage component
    Update imports, remove unused code, and optimize rendering for better performance.

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,11 +1,13 @@
-import getProducts from '@/actions/getProducts.action'
-import HeroImage from '@/components/HeroImage'
-import ProductList from '@/components/Products/ProductList'
-import Search from '@/components/Search'
-import { cn } from '@/lib/utils'
 import { Urbanist } from 'next/font/google'
 
-const font = Urbanist({ subsets: ['latin'] })
+import { getCategories } from '@/server/categories'
+import { getProductsDashboard } from '@/server/products'
+
+import { cn } from '@/lib/utils'
+
+import Categories from '@/components/Categories'
+import ProductList from '@/components/Products/ProductList'
+import Search from '@/components/Search'
 
 interface DashboardPageProps {
   searchParams: {
@@ -14,44 +16,27 @@ interface DashboardPageProps {
   }
 }
 
+const font = Urbanist({ subsets: ['latin'] })
+
 export default async function DashboardPage({
   searchParams
 }: DashboardPageProps) {
-  const products = await getProducts({ ...searchParams })
+  const products = await getProductsDashboard({ ...searchParams })
+  const categories = await getCategories()
 
   return (
     <>
       <main
         className={cn(
-          'flex flex-col items-center justify-center gap-y-4 px-4 md:px-6 lg:px-8',
+          'mx-auto flex min-h-screen max-w-7xl flex-col items-center gap-y-4 px-4 md:px-6 lg:px-8',
           font.className
         )}
       >
-        <HeroImage
-          image='/dashboard.png'
-          alt='Hero image'
-          aspectRatio={16 / 9}
-          width={1280}
-          height={720}
-          layout='responsive'
-          objectFit='cover'
-          objectPosition='center'
-          quality={100}
-          priority={true}
-          loading='eager'
-          className='w-full max-w-7xl py-4'
-        >
-          <h1 className='text-4xl font-black sm:text-5xl md:text-6xl lg:text-7xl'>
-            Welcome to the dashboard
-          </h1>
-          <p className='text-xs font-medium sm:text-sm md:text-lg'>
-            Find the products you need
-          </p>
-        </HeroImage>
-
-        <div className='w-full max-w-7xl py-4'>
-          <div className='flex items-center justify-between'>
+        <div className='w-full space-y-8 py-4'>
+          <div className='flex items-center justify-between gap-x-8'>
             <h2 className='text-2xl font-bold md:text-4xl'>Products</h2>
+
+            <Categories categories={categories} />
 
             <Search />
           </div>
