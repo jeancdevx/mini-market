87ea07888d0db61cc5c3b477d836a59fd79c29cf
jcodev2 <jcode2006@gmail.com>
2024-05-24 21:01:42 -0500

    refactor: update Categories component with Pagination
    Refactor Categories component to include the Pagination component

--- components/Categories/Categories.tsx
+++ components/Categories/Categories.tsx
@@ -1,10 +1,18 @@
 'use client'
 
-import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
 
+import { cn } from '@/lib/utils'
 import { useQueryString } from '@/hooks/use-query-string.hook'
 
-import { Button } from '@/components/ui/Button'
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious
+} from '@/components/ui/Pagination'
 
 interface CategoriesProps {
   categories: Array<{ id: string; name: string; createdAt: string }>
@@ -12,26 +20,109 @@ interface CategoriesProps {
 
 const Categories = ({ categories }: CategoriesProps) => {
   const { pathname, createQueryString, removeQueryString } = useQueryString()
+  const searchParams = useSearchParams()
+
+  const categoryId = searchParams.get('categoryId')
+
+  const paginatePrevious = () => {
+    if (categories.length === 0) {
+      return pathname
+    }
+
+    const skip = searchParams.get('skip')
+    const take = searchParams.get('take')
+
+    if (!skip || !take) {
+      return `${pathname}${categoryId ? `?categoryId=${categoryId}` : ''}`
+    }
+
+    const newSkip = parseInt(skip) - 5
+
+    if (newSkip < 0) {
+      return `${pathname}${categoryId ? `?categoryId=${categoryId}` : ''}`
+    } else {
+      return `${pathname}?take=5&skip=${newSkip}${categoryId ? `&categoryId=${categoryId}` : ''}`
+    }
+  }
+
+  const paginateNext = () => {
+    if (categories.length === 0) {
+      return pathname
+    }
+
+    const skip = searchParams.get('skip')
+    const take = searchParams.get('take')
+
+    if (!skip || !take) {
+      return `${pathname}?take=5&skip=5${categoryId ? `&categoryId=${categoryId}` : ''}`
+    }
+
+    const newSkip = parseInt(skip) + 5
+
+    if (categories.length < 5) {
+      return `${pathname}${categoryId ? `?categoryId=${categoryId}` : ''}`
+    } else {
+      return `${pathname}?take=5&skip=${newSkip}${categoryId ? `&categoryId=${categoryId}` : ''}`
+    }
+  }
 
   return (
-    <div className='hidden w-full flex-1 gap-x-2 overflow-hidden md:flex'>
-      <Link href={pathname + '?' + removeQueryString('categoryId')}>
-        <Button variant='secondary' size='sm' className='font-semibold'>
-          Clear
-        </Button>
-      </Link>
+    <Pagination>
+      <PaginationContent className='hidden w-full justify-center space-x-4 md:flex'>
+        <PaginationItem>
+          <PaginationPrevious
+            href={paginatePrevious()}
+            className={cn(
+              'w-full',
+              searchParams.get('skip') === '0' ||
+                (searchParams.get('skip') === null &&
+                  'pointer-events-none opacity-50')
+            )}
+          />
+        </PaginationItem>
 
-      {categories.map(({ id, name }) => (
-        <Link
-          key={id}
-          href={pathname + '?' + createQueryString('categoryId', id)}
-        >
-          <Button variant='secondary' size='sm' className='font-semibold'>
-            {name}
-          </Button>
-        </Link>
-      ))}
-    </div>
+        <PaginationItem>
+          <PaginationLink
+            href={pathname}
+            className='w-full px-2 text-sm font-bold'
+            onClick={() => removeQueryString('categoryId')}
+          >
+            All
+          </PaginationLink>
+        </PaginationItem>
+
+        {categories.map(category => (
+          <PaginationItem key={category.id}>
+            <PaginationLink
+              href={
+                pathname + '?' + createQueryString('categoryId', category.id)
+              }
+              className='w-full px-2 text-sm font-semibold'
+            >
+              {category.name}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+
+        {categories.length === 0 && (
+          <PaginationItem>
+            <Pagination className='pointer-events-none w-full opacity-50'>
+              No categories found
+            </Pagination>
+          </PaginationItem>
+        )}
+
+        <PaginationItem>
+          <PaginationNext
+            href={paginateNext()}
+            className={cn(
+              'w-full',
+              categories.length === 0 && 'pointer-events-none opacity-50'
+            )}
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
   )
 }
 
